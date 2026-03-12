2143a1163552856a24a3fb71d1e778dca85ab171
NIKHILBHUSHAN <nikhilbhushan95824@gmail.com>
2026-03-12 15:30:12 +0530

    Use Case 5: Booking Request (First-Come-First-Served)

--- src/Book_My_Stay_App.java
+++ src/Book_My_Stay_App.java
@@ -1,60 +1,45 @@
 import java.util.*;
-class RoomInventory{
-    private HashMap<String,Integer> roomAvailability;
-    public RoomInventory(){
-        roomAvailability=new HashMap<>();
-        initializeInventory();
-    }
-    private void initializeInventory(){
-        roomAvailability.put("Single",5);
-        roomAvailability.put("Double",3);
-        roomAvailability.put("Suite",2);
+import java.util.concurrent.locks.ReentrantLock;
 
-
+class Reservation{
+    private String guestName,roomType;
+    public Reservation(String guestName,String roomType){
+        this.guestName=guestName;
+        this.roomType=roomType;
     }
-    public HashMap<String,Integer> getRoomAvailability(){
-        return roomAvailability;
+    public String getGuestName(){
+        return guestName;
     }
-    public void updateAvailability(String roomtype,int count){
-        roomAvailability.put(roomtype,count);
+    public String getRoomType(){
+        return roomType;
     }
-
 }
-class RoomSearchService{
-    public void searchAvailableRooms(RoomInventory rooms){
-        HashMap<String,Integer> availability=rooms.getRoomAvailability();
-        for (String room:availability.keySet()){
-            System.out.println("Room: "+room);
-            if(availability.get(room)>0) {
-                if (room.equals("Single")) {
-                    System.out.println("Beds: 1");
-                    System.out.println("Size: 250ft");
-                    System.out.println("Price per Night: 1500.0");
-                    System.out.println("Available Rooms: " + availability.get(room));
-                    System.out.println();
-                }else if (room.equals("Double")) {
-                    System.out.println("Beds: 2");
-                    System.out.println("Size: 400ft");
-                    System.out.println("Price per Night: 2500.0");
-                    System.out.println("Available Rooms: "+availability.get(room));
-                    System.out.println();
-                }else if (room.equals("Suite")){
-                    System.out.println("Beds: 3");
-                    System.out.println("Size: 750ft");
-                    System.out.println("Price per Night: 5000.0");
-                    System.out.println("Available Rooms: "+availability.get(room));
-                    System.out.println();
-                }
-
-            }
-        }
+class BookingRequestQueue{
+    private Queue<Reservation> requestQueue;
+    public BookingRequestQueue(){requestQueue=new LinkedList<>();}
+    public void addRequest(Reservation reservation){
+        requestQueue.offer(reservation);
+    }
+    public Reservation getNextRequest(){
+        return requestQueue.poll();
+    }
+    public boolean hasPendingRequest(){
+        return !requestQueue.isEmpty();
     }
 }
 public class Book_My_Stay_App {
     public static void main(String[] args){
-        RoomInventory rooms=new RoomInventory();
-        RoomSearchService search=new RoomSearchService();
-        System.out.println("Room search\n");
-        search.searchAvailableRooms(rooms);
+        System.out.println("Booking Queue Request:");
+        BookingRequestQueue bookingqueue=new BookingRequestQueue();
+        Reservation r1=new Reservation("Nikhil","Single");
+        Reservation r2=new Reservation("Sai","Double");
+        Reservation r3=new Reservation("Arjun","Suite");
+        bookingqueue.addRequest(r1);
+        bookingqueue.addRequest(r2);
+        bookingqueue.addRequest(r3);
+        while(bookingqueue.hasPendingRequest()){
+            Reservation r=bookingqueue.getNextRequest();
+            System.out.println("Processing booking for Guest: "+r.getGuestName()+", Room Type: "+r.getRoomType());
+        }
     }
 }
